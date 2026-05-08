d3e97c06fa834ae61983f6a424867234b6bd094a
YusufZak <141759579+YusufZak@users.noreply.github.com>
2026-05-08 08:24:38 -0400

    Update SQL insert statement to include name_on_order

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ if (ingredients_list and name_on_order):
 
     st.write(ingredients_string)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                     values ('""" + ingredients_string + """' , '""" + name_on_order + """')"""
 
     #st.write(my_insert_stmt)
