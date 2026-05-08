b93714a111a188a45e24f9e12c267400dac37d8f
YusufZak <141759579+YusufZak@users.noreply.github.com>
2026-05-08 08:56:54 -0400

    Display watermelon nutrition data in a dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,8 +50,8 @@ if (ingredients_list and name_on_order):
 # New sections to display smoothiefroot nutrition information
 import requests  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response.json()) 
-
+# st.text(smoothiefroot_response.json()) 
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
 
